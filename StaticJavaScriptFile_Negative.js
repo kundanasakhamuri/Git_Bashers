0c9ec1617eae34a94f7eb0c05055268bbe4cb006
kundanasakhamuri <60694277+kundanasakhamuri@users.noreply.github.com>
2020-02-07 16:47:16 +0530

    Add files via upload

---
+++ StaticJavaScriptFile_Negative.js
@@ -0,0 +1,13 @@
+var name = "John";
+
+	(function(){
+	  	console.log("The name is : " + name);
+
+	  	var name = "Jane";
+		if(1==1)
+		{
+			console.log("test");
+		}
+
+	  	console.log("The name is : " +name);
+	})();
